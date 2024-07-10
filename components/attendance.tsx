8d135feb958522d4b57f5cbdb4cf1a32ff242580
sunnyegg <53990968+sunnyegg@users.noreply.github.com>
2024-07-10 17:05:10 +0700

    Feat/0.7.0 (#29)
    * [0.6.1] RELEASE (#28)
    
    * add menu announcement (#18)
    
    * fix session channel point
    
    * add enable/disable so, add auto so
    
    * fix handle close auto so delay
    
    * send analytics
    
    * disable close analytics
    
    * fix #25, fix #26
    
    * export csv, fix analytics sending double, add twitch link

--- components/attendance.tsx
+++ components/attendance.tsx
@@ -1,12 +1,62 @@
-import { ChattersPresent } from "@/app/types";
+import { useEffect, useState } from "react";
 import dayjs from "dayjs";
 import Image from "next/legacy/image";
+import { ChattersPresent } from "@/app/types";
+import DownloadIcon from "@/public/download.svg";
+import { MY_SESSION } from "@/const/keys";
 
 export default function Attendance({
   chattersPresent,
 }: {
   chattersPresent: ChattersPresent;
 }) {
+  const [dataCSV, setDataCSV] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [filename, setFilename] = useState("")
+
+  useEffect(() => {
+    const session = localStorage.getItem(MY_SESSION) ? JSON.parse(localStorage.getItem(MY_SESSION) || "") : ""
+    setFilename(`${session.name}-attendance-`)
+  }, [])
+
+  useEffect(() => {
+    const data: any = []
+    const keys = Object.keys(chattersPresent)
+    for (const v of keys) {
+      data.push({
+        username: chattersPresent[v].username,
+        present_at: dayjs(chattersPresent[v].time).format('YYYY-MM-DD HH:mm:ss'),
+      })
+    }
+    setDataCSV(data)
+  }, [chattersPresent])
+
+  const exportCSV = async (data: any, filename: any) => {
+    setLoading(true)
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_APP_URL}/api/export-csv`,
+      {
+        body: JSON.stringify({
+          data,
+          filename: filename + dayjs().format('YYYY-MM-DD_HH-mm-ss')
+        }),
+        method: 'POST'
+      }
+    );
+
+    const { data: dataFilename } = await res.json()
+    const dataCSV = await fetch(`${process.env.NEXT_PUBLIC_APP_URL}/${dataFilename}`)
+    const blob = await dataCSV.blob()
+    const url = window.URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.href = url;
+    link.setAttribute('download', dataFilename);
+    document.body.appendChild(link);
+    link.click();
+    link?.parentNode?.removeChild(link);
+    setLoading(false)
+  }
+
   return (
     <section className="collapse collapse-arrow rounded-lg bg-base-200 px-2">
       <input type="checkbox" />
@@ -15,6 +65,29 @@ export default function Attendance({
       </div>
 
       <div className="collapse-content space-y-2 overflow-auto">
+        {
+          Object.entries(chattersPresent).length ? (
+            <button className="btn btn-ghost float-end" onClick={() => {
+              exportCSV(dataCSV, filename)
+            }}>
+              {loading ? (
+                <div className="loading loading-spinner loading-sm"></div>
+              ) : DownloadIcon ? (
+                <Image
+                  alt="download icon"
+                  src={DownloadIcon}
+                  width={20}
+                  height={20}
+                  className="dark:invert"
+                />
+              ) : (
+                ""
+              )}
+            </button>
+          ) : ""
+        }
+
+
         {Object.entries(chattersPresent).length ? Object.entries(chattersPresent).map((chatter, idx) => {
           return (
             <div key={idx} className="flex items-center justify-between rounded-lg">

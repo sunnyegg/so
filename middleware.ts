529b7e282dd39333315e2d9d6926761e58d6dce5
sunnyegg <adilatresna21@gmail.com>
2024-09-18 17:51:11 +0700

    add middleware rate-limit

--- middleware.ts
+++ middleware.ts
@@ -2,8 +2,13 @@ import { NextRequest, NextResponse } from "next/server";
 
 import corsMiddleware from "./middlewares/cors";
 import authenticationMiddleware from "./middlewares/authentication";
+import rateLimitMiddleware from "./middlewares/rate-limit";
 
-const availableMiddlewares = [corsMiddleware, authenticationMiddleware];
+const availableMiddlewares = [
+  corsMiddleware,
+  authenticationMiddleware,
+  rateLimitMiddleware,
+];
 
 export function middleware(request: NextRequest) {
   for (const m of availableMiddlewares) {
---
+++ middlewares/rate-limit.ts
@@ -0,0 +1,46 @@
+import { NextRequest, NextResponse } from "next/server";
+
+const rateLimitClients = new Map<
+  string,
+  {
+    count: number;
+    lastReset: number;
+  }
+>();
+
+export default function rateLimitMiddleware(request: NextRequest) {
+  const ip = request.headers.get("x-forwarded-for");
+  const limit = 10;
+  const interval = 60 * 1000; // 1 minute
+
+  if (!ip) {
+    return NextResponse.next();
+  }
+
+  if (!rateLimitClients.has(ip)) {
+    rateLimitClients.set(ip, {
+      count: 0,
+      lastReset: Date.now(),
+    });
+  }
+
+  const ipData = rateLimitClients.get(ip)!;
+  if (Date.now() - ipData.lastReset > interval) {
+    ipData.count = 0;
+    ipData.lastReset = Date.now();
+  } else {
+    ipData.count += 1;
+  }
+
+  if (ipData.count >= limit) {
+    return Response.json(
+      {
+        status: false,
+        error: "Too many requests",
+      },
+      { status: 429 }
+    );
+  }
+
+  return NextResponse.next();
+}

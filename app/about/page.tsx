b6221f2b7509aab76357f17ab226d06e3dbcb81a
sunnyegg <adilatresna21@gmail.com>
2024-09-19 13:30:08 +0700

    add missing pages

---
+++ app/about/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import TopBar from "@/components/common/topbar";
+
+export default function AboutPage() {
+  return (
+    <div className="mx-4 my-8 md:mx-32">
+      <TopBar />
+
+      <div>About</div>
+    </div>
+  );
+}
---
+++ app/how-it-works/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import TopBar from "@/components/common/topbar";
+
+export default function HowItWorksPage() {
+  return (
+    <div className="mx-4 my-8 md:mx-32">
+      <TopBar />
+
+      <div>How It Works</div>
+    </div>
+  );
+}
